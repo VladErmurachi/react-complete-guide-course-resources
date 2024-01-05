805913f68e7dc076fd2bb738fd9f4a1ab67f9842
vermurachi <vlad.ermurachi@endava.com>
2024-01-05 15:10:50 +0200

    Implement first app

--- code/05 Essentials Practice/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,7 +1,34 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import {useState} from "react";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: +newValue
+            }
+        });
+    }
+
+    return (
+        <>
+            <Header/>
+            <UserInput onChange={handleChange}
+                       userInput={userInput}/>
+            <Results input={userInput}/>
+        </>
+    )
+
 }
 
 export default App
